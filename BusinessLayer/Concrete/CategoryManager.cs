c9e092c8978dbf8f139a6c6f1411684c7da73610
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-04-25 02:43:19 +0300

    Ders 19 Business Layer tamamlandı. CategoryManager BLL projesine eklendi.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAllBLL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBLL(Category p)
+        {
+            if(p==null || p.CategoryName == string.Empty || p.CategoryName.Length<=3 || p.CategoryDescription==string.Empty||p.CategoryName.Length>50)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+
+        }
+    }
+}
