3ef3bede521b41def8ee49144c956be615db1b22
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-04-24 23:34:00 +0300

    Ders 17 Generic Interface tamamlandı. IRepository interface tamamlandı. Tüm sınıflar için ayrı Dal interface leri tamamlandı. WriterRepository için örnek implementasyon gerçekleştirildi.

--- DataAccessLayer/Abstract/IContactDal.cs
+++ DataAccessLayer/Abstract/IContactDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    interface ICategoryDal : IRepository<Category>
+    interface IContactDal:IRepository<Contact>
     {
     }
 }
--- DataAccessLayer/Abstract/IHeadingDal.cs
+++ DataAccessLayer/Abstract/IHeadingDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    interface ICategoryDal : IRepository<Category>
+    interface IHeadingDal:IRepository<Heading>
     {
     }
 }
